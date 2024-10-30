0ed6ab2431e0750da403432f73054232c66caec1
mert karaömer <mert.karaomer@ogr.sakarya.edu.tr>
2024-10-30 15:54:22 +0300

    fix

--- MODUL_BUL/Form1.Designer.cs
+++ MODUL_BUL/Form1.Designer.cs
@@ -90,6 +90,7 @@
             // 
             advancedDataGridView1.AllowUserToAddRows = false;
             advancedDataGridView1.AllowUserToDeleteRows = false;
+            advancedDataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             advancedDataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             advancedDataGridView1.BackgroundColor = Color.White;
             advancedDataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
